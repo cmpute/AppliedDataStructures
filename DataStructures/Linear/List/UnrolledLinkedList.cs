07a60f7b6788a312f6fc407485c2a72704e500f0
Jacob C <cmpute@qq.com>
2017-01-27 14:03:28 +0800

    Unroll Linked List Partially Finished

---
+++ DataStructures/Linear/List/UnrolledLinkedList.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Collections.Advanced
+{
+    public class UnrolledLinkedList<TNode, TData> : IList<TData>, IPrintable
+        where TNode : UnrolledLinkedListNode<TData>
+    {
+        private const int _defaultCapacity = 4;
+
+        Func<int, TNode> _new;
+        UnrolledLinkedListNode<TData> head;
+        int _version;
+        int blocksize;
+
+        public int Capacity => blocksize * blocksize;
+
+        public UnrolledLinkedList(Func<int, TNode> newNode) : this(newNode, _defaultCapacity) { }
+        public UnrolledLinkedList(Func<int, TNode> newNode, int capacity)
+        {
+            _new = newNode;
+            blocksize = (int)Math.Ceiling(Math.Sqrt(capacity));
+        }
+
+        private Tuple<UnrolledLinkedListNode<TData>, int> Find(int index)
+        {
+            if (index <= Count - index) // Find orderly faster
+            {
+                var current = head;
+                while (index >= current.Count)
+                {
+                    index -= current.Count;
+                    current = current.Next;
+                }
+                return new Tuple<UnrolledLinkedListNode<TData>, int>(current, index);
+            }
+            else // Find reversely faster
+            {
+                index = Count - index;
+                var current = head.Previous;
+                while (index > current.Count)
+                {
+                    index -= current.Count;
+                    current = current.Previous;
+                }
+                return new Tuple<UnrolledLinkedListNode<TData>, int>(current, current.Count - index);
+            }
+        }
+
+        public void OperateAt(int index, ActionRef<TData> operation)
+        {
+            var res = Find(index);
+            var data = res.Item1.Items[res.Item2];
+            operation(ref data);
+            res.Item1.Items[res.Item2] = data;
+        }
+
+        struct Enumerator : IEnumerator<TData>
+        {
+            UnrolledLinkedList<TNode, TData> list;
+            UnrolledLinkedListNode<TData> cnode;
+            TData current;
+            int index;
+            int version;
+
+            public Enumerator(UnrolledLinkedList<TNode, TData> list)
+            {
+                this.list = list;
+                cnode = null;
+                current = default(TData);
+                index = 0;
+                version = list._version;
+            }
+
+            public TData Current => current;
+
+            object IEnumerator.Current => Current;
+
+            public void Dispose() { }
+
+            public bool MoveNext()
+            {
+                if (version != list._version)
+                    throw new InvalidOperationException("在枚举过程中集合被修改过");
+
+                if (cnode == null)
+                {
+                    cnode = list.head;
+                    if (list.Count == 0) return false;
+                    current = cnode.Items[index++];
+                    return true;
+                }
+                else
+                {
+                    if (index < cnode.Count)
+                    {
+                        current = cnode.Items[index++];
+                        return true;
+                    }
+                    else
+                    {
+                        if (cnode.Next == list.head) return false;
+                        cnode = cnode.Next;
+                        index = 0;
+                        current = cnode.Items[index++];
+                        return true;
+                    }
+                }
+            }
+
+            public void Reset()
+            {
+                if (version != list._version)
+                    throw new InvalidOperationException("在枚举过程中集合被修改过");
+                current = default(TData);
+                cnode = null;
+            }
+        }
+
+        struct BackEnumerator : IEnumerator<TData>
+        {
+            UnrolledLinkedList<TNode, TData> list;
+            UnrolledLinkedListNode<TData> cnode;
+            TData current;
+            int index;
+            int version;
+
+            public BackEnumerator(UnrolledLinkedList<TNode, TData> list)
+            {
+                this.list = list;
+                cnode = null;
+                current = default(TData);
+                index = 0;
+                version = list._version;
+            }
+
+            public TData Current => current;
+
+            object IEnumerator.Current => current;
+
+            public void Dispose() { }
+
+            public bool MoveNext()
+            {
+                if (version != list._version)
+                    throw new InvalidOperationException("在枚举过程中集合被修改过");
+
+                if (cnode == null)
+                {
+                    cnode = list.head.Previous;
+                    index = cnode.Count - 1;
+                    if (list.Count == 0) return false;
+                    current = cnode.Items[index--];
+                    return true;
+                }
+                else
+                {
+                    if (index >= 0)
+                    {
+                        current = cnode.Items[index--];
+                        return true;
+                    }
+                    else
+                    {
+                        if (cnode == list.head) return false;
+                        cnode = cnode.Previous;
+                        index = cnode.Count - 1;
+                        current = cnode.Items[index--];
+                        return true;
+                    }
+                }
+            }
+
+            public void Reset()
+            {
+                if (version != list._version)
+                    throw new InvalidOperationException("在枚举过程中集合被修改过");
+                current = default(TData);
+                cnode = null;
+            }
+        }
+
+        #region IList & IPrintable implementation
+
+        /// <summary>
+        /// Get or set the value at <paramref name="index"/>. If get and set at the same time, use <see cref="OperateAt(int, ActionRef{TData})"/> method to improve efficiency.
+        /// 获取或设置列表在<paramref name="index"/>的值，若同时获取和设置，请使用<see cref="OperateAt(int, ActionRef{TData})"/>方法以提高效率。
+        /// </summary>
+        /// <param name="index">需要获取或设置的对象的索引</param>
+        public TData this[int index]
+        {
+            get
+            {
+                var res = Find(index);
+                return res.Item1.Items[res.Item2];
+            }
+            set
+            {
+                var res = Find(index); // can be improved by add log of last operation
+                res.Item1.Items[res.Item2] = value;
+            }
+        }
+        
+        public int Count { get; protected set; }
+
+        public bool IsReadOnly => false;
+
+        public void Add(TData item)
+        {
+            if (Count == 0)
+            {
+                head = _new(blocksize << 1);
+                head.Next = head;
+            }
+            head.Previous.Insert(head.Previous.Count, item, _new);
+
+            Count++;
+        }
+
+        public void Clear()
+        {
+            head = null;
+            Count = 0;
+            _version++;
+        }
+
+        public bool Contains(TData item) => IndexOf(item) >= 0;
+
+        public void CopyTo(TData[] array, int arrayIndex)
+        {
+            int current = arrayIndex;
+            var iter = GetEnumerator();
+            while (iter.MoveNext())
+                array[current++] = iter.Current;
+        }
+
+        public IEnumerator<TData> GetEnumerator(bool reverse)
+        {
+            if (reverse)
+                return new BackEnumerator(this);
+            else
+                return new Enumerator(this);
+        }
+        public IEnumerator<TData> GetEnumerator() => new Enumerator(this);
+
+        public virtual int IndexOf(TData item) => EnumerateIndexOf(item, false);
+
+        public virtual int LastIndexOf(TData item) => EnumerateIndexOf(item, true);
+
+        public virtual int EnumerateIndexOf(TData item, bool reverse)
+        {
+            var itor = GetEnumerator(reverse);
+            int count = 0;
+            if ((Object)item == null)
+            {
+                while (itor.MoveNext())
+                {
+                    if ((object)itor.Current == null)
+                        return reverse ? Count - count - 1 : count;
+                    count++;
+                }
+            }
+            else
+            {
+                var comparer = EqualityComparer<TData>.Default;
+                while (itor.MoveNext())
+                {
+                    if (comparer.Equals(itor.Current, item))
+                        return reverse ? Count - count - 1 : count;
+                    count++;
+                }
+            }
+            return -1;
+        }
+
+        public void Insert(int index, TData item)
+        {
+            var loc = Find(index);
+            loc.Item1.Insert(loc.Item2, item, _new);
+            Count++;
+        }
+
+        public bool Remove(TData item)
+        {
+            var index = IndexOf(item);
+            if (index < 0) return false;
+
+            RemoveAt(index);
+            return true;
+        }
+
+        public void RemoveAt(int index)
+        {
+            var loc = Find(index);
+            loc.Item1.Delete(loc.Item2);
+            Count--;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public void PrintTo(TextWriter textOut)
+        {
+            if (Count == 0) textOut.WriteLine("Null Unrolled Linked List");
+
+            var node = head;
+            node.PrintTo(textOut);
+
+            while (node.Next != head)
+            {
+                node = node.Next;
+                textOut.Write("->");
+                node.PrintTo(textOut);
+            }
+        }
+
+        #endregion
+
+        #region Range Operations
+        #endregion
+    }
+}
---
+++ DataStructures/Linear/List/UnrolledLinkedListNode.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Collections.Advanced
+{
+    public class UnrolledLinkedListNode<TData> : IPersistent, IMergable<UnrolledLinkedListNode<TData>>, IPrintable
+    {
+        bool _rmref; //whether to remove reference, not necessary for value type
+
+        public TData[] Items { get; protected set; }
+
+        UnrolledLinkedListNode<TData> _prev, _next;
+        public UnrolledLinkedListNode<TData> Previous
+        {
+            get { return _prev; }
+            set
+            {
+                _prev = value;
+                if (value != null) value._next = this;
+            }
+        }
+        public UnrolledLinkedListNode<TData> Next
+        {
+            get { return _next; }
+            set
+            {
+                _next = value;
+                if (value != null) value._prev = this;
+            }
+        }
+
+        public UnrolledLinkedListNode(int capacity)
+        {
+            Items = new TData[capacity];
+#if DEBUG
+            _rmref = true;
+#else
+            _rmref = !typeof(TData).IsValueType;
+#endif
+        }
+
+        public int Capacity => Items.Length;
+        public virtual float MergeRatio => 0.3f;
+        public int Count { get; protected set; } = 0;
+        public int Version { get; protected set; }
+
+        public void Split(Func<int, UnrolledLinkedListNode<TData>> newNode) => Split(Count >> 1, newNode);
+        public void Split(int splitIndex, Func<int, UnrolledLinkedListNode<TData>> newNode)
+        {
+            var newnext = newNode(Capacity);
+            newnext.Next = Next;
+            newnext.Previous = this;
+
+            newnext.Count = Count - splitIndex;
+            Count = splitIndex;
+            Array.Copy(Items, Count, newnext.Items, 0, newnext.Count);
+            if(_rmref) Array.Clear(Items, Count, newnext.Count);
+
+            newnext.Update();
+        }
+
+        public void Merge(UnrolledLinkedListNode<TData> newNext) => Merge(newNext, 0);
+        /// <summary>
+        /// Merge current node with <paramref name="newNext"/>, the nodes between this and <paramref name="newNext"/> are abandoned.
+        /// 合并当前结点与<paramref name="newNext"/>， 之间的结点将会被抛弃
+        /// </summary>
+        /// <param name="newNext">需要被合并的结点，请保证它是当前结点的后继</param>
+        /// <param name="startIndex">合并的结点开始合并的数据位置</param>
+        public void Merge(UnrolledLinkedListNode<TData> newNext, int startIndex)
+        {
+            if (newNext.Count > 0)
+            {
+                var copycount = newNext.Count - startIndex;
+                Array.Copy(newNext.Items, startIndex, Items, Count, copycount);
+                if (_rmref) Array.Clear(newNext.Items, 0, startIndex);
+                Count += copycount;
+            }
+
+            Next = newNext.Next;
+
+            Update();
+        }
+
+        public void Insert(int index, TData item, Func<int, UnrolledLinkedListNode<TData>> newNext)
+        {
+            if (Count == Capacity)
+            {
+                Split(newNext);
+                if (index > Count)
+                {
+                    Next.Insert(index - Count, item, newNext);
+                    return;
+                }
+            }
+            
+            if (index < Count)
+                Array.Copy(Items, index, Items, index + 1, Count - index);
+            Items[index] = item;
+            Count++;
+
+            Update();
+        }
+
+        public TData Delete(int index)
+        {
+            var res = Items[index];
+            if (index < --Count)
+            {
+                Array.Copy(Items, index + 1, Items, index, Count - index);
+                if (_rmref) Items[Count] = default(TData);
+            }
+
+            if (!FixDown())
+                Update();
+
+            return res;
+        }
+
+        private bool FixDown()
+        {
+            bool fix = false;
+            if (Count < Capacity * MergeRatio)
+            {
+                if (Next != this && Count + Next.Count <= Capacity)
+                {
+                    Merge(Next);
+                    fix = true;
+                }
+                if (Previous != this && Count + Previous.Count <= Previous.Capacity)
+                {
+                    Previous.Merge(this);
+                    fix = true;
+                }
+            }
+
+            return fix;
+        }
+
+        protected virtual void Update() { Version++; }
+
+        public void PrintTo(TextWriter textOut)
+        {
+            if (Count == 0) textOut.Write("[null node]");
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append('[');
+            sb.Append(Items[0]);
+
+            for (int i = 1; i < Count; i++)
+            {
+                sb.Append(' ');
+                sb.Append(Items[i]);
+            }
+            sb.Append(']');
+
+            textOut.Write(sb.ToString());
+        }
+    }
+}
